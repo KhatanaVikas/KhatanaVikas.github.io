7b8568c7b7a644461ad91baf94cbf6020c6a9d4b
Vikas Khatana <34890007+KhatanaVikas@users.noreply.github.com>
2019-06-16 14:01:24 +0530

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,7 +16,7 @@ class DefaultController extends Controller
      * Description : Home page for this project
      * @param Request $request
      * @return Response
-     * @Route("/", name="homepage")
+     * @Route("/vikas", name="homepage")
      */
     public function indexAction(Request $request)
     {
